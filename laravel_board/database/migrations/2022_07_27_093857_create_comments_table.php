3d0d5dbdac5f302c3bd2977797729c88fc3b8a04
ajk78978 <ajk78978@gmail.com>
2022-07-27 19:06:48 +0900

    create model , migration

--- laravel_board/database/migrations/2022_07_27_093857_create_comments_table.php
+++ laravel_board/database/migrations/2022_07_27_093857_create_comments_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('board_id');
+            $table->integer('parent_comment_id');
+            $table->unsignedTinyInteger('class');
+            $table->longText('contents');
+            $table->integer('register_user_id');
+            $table->integer('order')->comment('order: comment created_at asc');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
@@ -32,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 };
